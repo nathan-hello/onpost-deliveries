0c0bd1a6d5d3e9452fab500e83339ba44d00e94d
nathan-hello <natey3k@gmail.com>
2024-11-10 12:49:05 -0500

    moved formAction to form action=

--- src/app/(auth-pages)/protected/reset-password/page.tsx
+++ src/app/(auth-pages)/protected/reset-password/page.tsx
@@ -9,7 +9,10 @@ export default async function ResetPassword(props: {
 }) {
   const searchParams = await props.searchParams;
   return (
-    <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
+    <form
+      action={resetPasswordAction}
+      className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4"
+    >
       <h1 className="text-2xl font-medium">Reset password</h1>
       <p className="text-sm text-foreground/60">
         Please enter your new password below.
@@ -28,9 +31,7 @@ export default async function ResetPassword(props: {
         placeholder="Confirm password"
         required
       />
-      <SubmitButton formAction={resetPasswordAction}>
-        Reset password
-      </SubmitButton>
+      <SubmitButton>Reset password</SubmitButton>
       <FormMessage message={searchParams} />
     </form>
   );
--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
-    <form className="flex-1 flex flex-col min-w-64">
+    <form action={signInAction} className="flex-1 flex flex-col min-w-64">
       <h1 className="text-2xl font-medium">Sign in</h1>
       <p className="text-sm text-foreground">
         Don't have an account?{" "}
@@ -34,9 +34,7 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
           placeholder="Your password"
           required
         />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
-        </SubmitButton>
+        <SubmitButton pendingText="Signing In...">Sign in</SubmitButton>
         <FormMessage message={searchParams} />
       </div>
     </form>
